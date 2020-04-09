4238fd849a8059ad08d9f4500c01396b9c5645c2
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-04-09 11:16:08 -0500

    added text population on page load

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
-var timeBlocks = $(".time-block").toArray();
 var hourlyContent = [];
+// If there is hourlyContent data in localStorage, put it in an array
+if(localStorage.getItem("hourlyContent") !== null) {
+	hourlyContent = localStorage.getItem("hourlyContent").split(",")
+}
 
 // Display current day month and year at top
 $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
 
-// Function to determine what blocks are past present and future
-timeBlocks.forEach(function(block) {
+// Function to determine what blocks are past present and future and to populate list with text out of localStorage
+$(".time-block").toArray().forEach(function(block) {
 	// Check data hour against current 24-hour-time hour (negates the need for AM/PM checking)
 	if(parseInt(block.getAttribute("data-hour")) < parseInt(moment().format("HH"))) {
 		block.classList.add("past");
@@ -13,19 +16,25 @@ timeBlocks.forEach(function(block) {
 		block.classList.add("present");
 	} else {
 		block.classList.add("future");
-	}		
+	}
+
+	block.children[1].textContent = hourlyContent[parseInt(block.getAttribute("data-hour"))];
 });
 
-// Function to save text content when lock button is clicked
-$(".saveBtn").click(function() {
-	// Get hour-data from parent time-block
-	var hourToSave = parseInt(this.parentElement.getAttribute("data-hour"));
 
-	// Get text from associated timeblock and save in that hours index of array
-	hourSchedule = this.parentElement.children[1].textContent;
-	hourlyContent[hourToSave] =  hourSchedule;
-	console.log(hourlyContent);
 
-	// Save array to localStorage
-	localStorage.setItem("hourlyContent", hourlyContent.toString());
+
+$(document).ready(function() {
+	// Function to save text content when lock button is clicked
+	$(".saveBtn").click(function() {
+		// Get hour-data from parent time-block
+		var hourToSave = parseInt(this.parentElement.getAttribute("data-hour"));
+		
+		// Get text from associated timeblock and save in that hours index of array
+		hourSchedule = this.parentElement.children[1].textContent;
+		hourlyContent[hourToSave] =  hourSchedule;
+		
+		// Save array to localStorage
+		localStorage.setItem("hourlyContent", hourlyContent.toString());
+	});
 });
