55f81848d0d50bc4efd3f8906ed630a1817358cb
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-04-08 23:44:53 -0500

    made timeblocks responsive to current time

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
+var timeBlocks = $(".time-block").toArray();
+
 $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
+
+timeBlocks.forEach(function(block) {
+	if(parseInt(block.getAttribute("data-hour")) < parseInt(moment().format("HH"))) {
+		block.classList.add("past");
+	} else if(parseInt(block.getAttribute("data-hour")) = parseInt(moment().format("HH"))) {
+		block.classList.add("present");
+	} else {
+		block.classList.add("future");
+	}		
+});
